1415f5e3109a65b09009604a6b70f4e3193e05cc
yutaka-max <147456481+yutaka-max@users.noreply.github.com>
2023-10-11 17:41:49 +0900

    128,commonjs修正　その他新規追加　yutaka-max

--- 01_プロトタイプ/site/js/common.js
+++ 01_プロトタイプ/site/js/common.js
@@ -7,8 +7,17 @@ function tr_click(num){
 
   //詳細ページに移動
   window.location.href = 'dummy.html';
-
-
 }
+
+function menu_click(hoge){
+  alert("aaa");
+  //遷移するメニューIDを取得
+  let url_go = "menu_" + hoge + ".html";
+  alert(url_go);
+
+  //詳細ページに移動
+  window.location.href = url_go;
+}
+
 
 
