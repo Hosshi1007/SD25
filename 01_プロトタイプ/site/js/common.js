340ee7accdf8c9217686d8f597e4a6735506b9e9
づっちゃん <147456555+ducchan@users.noreply.github.com>
2023-10-19 21:45:17 +0900

    HTML追加＆CSS（kaiin.css）追加＆ js記述追加　工藤ですにょん

--- 01_プロトタイプ/site/js/common.js
+++ 01_プロトタイプ/site/js/common.js
@@ -17,7 +17,16 @@ function menu_click(hoge){
   //詳細ページに移動
   window.location.href = url_go;
 }
-
+function appoint_click(num){
+  //これは僕ちんの術式だにょ～～～～～～～～～NNNN（工藤）
+  let elements = document.getElementsByClassName("findID");
+  //会員ID格納
+  let id = elements.item(0).rows[num].cells[0].firstChild.data;
+  alert(id);
+  //詳細ページに移動
+  window.location.href = '6.html';
+  //これは僕ちんの術式だにょ～～～～～～～～～NNNN（工藤）
+}
 (() => {
   //HTMLのid値を使って以下のDOM要素を取得
   const downbutton = document.getElementById('down');
